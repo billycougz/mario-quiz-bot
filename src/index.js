348e1ca26d57f55cc864a86a00f25c73bc886dba
billycougz <billycougz@gmail.com>
2023-05-25 22:14:58 -0400

    Cleaned up handler

--- src/index.js
+++ src/index.js
@@ -1,40 +1,16 @@
 const { handleNewQuiz, handleQuizResults } = require('./handlers');
 
 exports.handler = async (event, context) => {
-	// console.log('Received event:', JSON.stringify(event, null, 2));
-	const isFunctionUrl = Boolean(event.requestContext?.http);
-	if (isFunctionUrl && process.env.STAGE && process.env.STAGE !== 'PROD') {
-		const eventName = event.queryStringParameters?.eventName;
-		let body = {};
-		switch (eventName) {
-			case 'newQuiz':
-				body = await handleNewQuiz(event);
-				break;
-			case 'quizResults':
-				body = await handleQuizResults(event);
-				break;
-			default:
-				console.error('URL events require an eventName query param.');
-		}
-		return {
-			statusCode: 200,
-			body: JSON.stringify(body),
-		};
+	console.log('Lambda Event', event);
+	switch (event.detail?.eventName) {
+		case 'newQuiz':
+			await handleNewQuiz(event);
+			break;
+		case 'quizResults':
+			await handleQuizResults(event);
+			break;
+		default:
+			const errorReason = event.detail?.eventName ? 'invalid' : 'empty';
+			console.error(`The eventName is ${errorReason}.`);
 	}
-	const isScheduled = event['detail-type'] === 'Scheduled Event';
-	if (isScheduled) {
-		const { eventName } = event.detail;
-		switch (eventName) {
-			case 'newQuiz':
-				await handleNewQuiz(event);
-				break;
-			case 'quizResults':
-				await handleQuizResults(event);
-				break;
-			default:
-				console.error('Scheduled events require an eventName.');
-		}
-		return;
-	}
-	console.error(`Event type did not match any condition.`);
 };
